c76aa71903790481410ce70c6510a2cd937998b3
Nguyen811 <trinhhoainguyenn@gmail.com>
2022-05-02 00:36:31 +0700

    Bai 16: Tim kiem tren mang 1 chieu

--- src/lthdt/chuong3/donglenh/testProductArray.java
+++ src/lthdt/chuong3/donglenh/testProductArray.java
@@ -23,6 +23,7 @@ public class testProductArray {
         //Tao mang cac san pham
         Product[] sp = new Product[]{new Product("Sunsilk",12),
                                         new Product("Lifebouy", 5),
+                                        new Product("Sunsilk",8),
                                         new Product("Espresso", 3),
                                         new Product("Thien long", 2)};
         System.out.println("Mang ban dau la");
@@ -37,12 +38,21 @@ public class testProductArray {
 //                }
         
         //Sap xep mang theo gia ca tang dan
-        Arrays.sort(sp, new ProductCompByPrice());
-        System.out.println("Mang sap xep tang dan theo gia ca");
-        System.out.println(Arrays.toString(sp));
-        System.out.println("Mang sap xep tang dan theo ten san pham");
+//        Arrays.sort(sp, new ProductCompByPrice());
+//        System.out.println("Mang sap xep tang dan theo gia ca");
+//        System.out.println(Arrays.toString(sp));
+        //Sap xep mang theo ten san pham tang dan
         Arrays.sort(sp, new ProductCompByName());
+        System.out.println("Mang sap xep tang dan theo ten san pham");
         System.out.println(Arrays.toString(sp));
+        
+        int index = Arrays.binarySearch(sp, new Product("Sunsilk", 12), new ProductCompByName());
+        if(index < 0){
+            System.out.println("Khong tim thay");
+        }else{
+            System.out.println("Phan tu thu " + index + " chua gia tri can tim");
+            System.out.println(index);
+        }
     }
     
 }
